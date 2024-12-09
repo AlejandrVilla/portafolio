ad7d913ba2e9caa62d1926f1d8d75f0e808e75b8
AlejandrVilla <avillah@unsa.edu.pe>
2024-12-09 12:19:19 -0500

    object fit for cards

--- src/components/specific/TechItem.jsx
+++ src/components/specific/TechItem.jsx
@@ -2,7 +2,7 @@ const TechItem = ( props ) => {
     return(
         <div className="icon-div">
             <img 
-                style={{width:'100px', height:'100px'}} 
+                style={{width:'100px', height:'100px', objectFit:"contain"}} 
                 src={props.path} 
                 alt={props.alt} />
         </div>
